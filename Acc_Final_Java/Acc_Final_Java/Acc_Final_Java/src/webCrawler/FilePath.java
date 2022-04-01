adaea53bea289688755b258248c94939da57a299
Raman <bhathalr@uwindsor.ca>
2022-03-31 23:01:51 -0400

    Made some changes in file structure

--- Acc_Final_Java/Acc_Final_Java/Acc_Final_Java/src/webCrawler/FilePath.java
+++
@@ -1,7 +0,0 @@
-package webCrawler;
-
-public class FilePath {
-	public static String textFile="data/TextPage/";
-	public static String HtmlFile="data/HTMLPage/";
-	public static String dictionaryFile="data/Dictionary/";
-}
